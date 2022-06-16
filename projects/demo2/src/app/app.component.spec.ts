d63d44944daaa9caf713201d7574d6dc068d7dbf
Sumathie Sundaresan <sumathie.sundaresan@sportngin.com>
2022-06-16 11:16:03 -0700

    Added diff demos

--- projects/demo2/src/app/app.component.spec.ts
+++ projects/demo2/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'demo2'`, () => {
+  it(`should have as title 'ld-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('demo2');
+    expect(app.title).toEqual('ld-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('demo2 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ld-app app is running!');
   });
 });
--- projects/demo2/src/app/app.module.ts
+++ projects/demo2/src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrderManagerComponent } from './order-manager/order-manager.component';
+import { OrderDetailsComponent } from './order-details/order-details.component';
+import { LaunchDarklyService } from './launch-darkly-service';
+import { OrderListComponent } from './order-list/order-list.component';
+import { OrderHeaderComponent } from './order-header/order-header.component';
+import { OrderItemsComponent } from './order-items/order-items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderManagerComponent,
+    OrderDetailsComponent,
+    OrderDetailsComponent,
+    OrderListComponent,
+    OrderHeaderComponent,
+    OrderItemsComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [LaunchDarklyService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
